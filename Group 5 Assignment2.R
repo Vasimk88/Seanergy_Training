a352a19adc9164a968c4ce7254b1804ab706f759
satya1322 <46417499+satya1322@users.noreply.github.com>
2019-01-31 08:53:13 +0530

    Group 5 Assignment2
    Group 5 Assignment2

--- Group 5 Assignment2.R
+++ Group 5 Assignment2.R
@@ -40,3 +40,94 @@ plot(model2)
 
 termplot(model2)
 summary(model2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##  Assumptions of linear regression
+
+# 1. The mean of residuals is zero
+airquality
+
+#mod <- lm(dist ~ speed, data=cars)
+mean(model2$residuals)
+
+# Assumption 3: Homoscedasticity of residuals or equal variance
+
+par(mfrow=c(2,2))  # set 2 rows and 2 column plot layout
+#mod_1 <- lm(mpg ~ disp, data=mtcars)  # linear model
+#plot(mod_1)
+
+#In this case, there is a definite pattern noticed.
+#So, there is heteroscedasticity. Lets check this on a different model.
+
+#mod <- lm(dist ~ speed, data=cars[1:20, ])  #  linear model
+plot(model2)
+
+#Now, the points appear random and the line looks pretty flat, with no increasing or decreasing trend.
+#So, the condition of homoscedasticity can be accepted.
+
+# Assumption 4: The X variables and residuals are uncorrelated
+
+##Do a correlation test on the X variable and the residuals.
+
+#mod.lm <- lm(dist ~ speed, data=cars)
+cor.test(model1$residuals, model2$residuals)  # do correlation test 
+
+
+#p-value is high, so null hypothesis that true correlation is 0 can't be rejected.
+#So, the assumption holds true for this model.
+
+# Assumption 5: The number of observations must be greater than number of Xs
+
+##This can be directly observed by looking at the data.
+
+# Assumption 6: The variability in X values is positive
+
+##This means the X values in a given sample must not all be the same (or even nearly the same).
+
+var(airquality$Temp)  
+
+# The variance in the X variable above is much larger than 0. So, this assumption is satisfied.
+
+# Assumption 8: The regression model is correctly specified
+
+##This means that if the Y and X variable has an inverse relationship,
+##the model equation should be specified appropriately:
+
+
+## Y=??1+??2???(1/X)
+
+# Assumption 8: No perfect multicollinearity
+
+## If the VIF of a variable is high, it means the information in that variable is already
+##explained by other
+## X variables present in the given model, which means, more redundant is that variable.
+
+# VIF=1/(1???Rsq)
+
+## where, Rsq is the Rsq term for the model with given X as
+## response against all other Xs that went into the model as predictors.
+
+
+
+
+
+
+
+
+
+
+
+
+
